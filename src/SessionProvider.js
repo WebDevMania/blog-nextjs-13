395c834ff32b757bc398dd418a5fc01223d6a777
Villy <villyofficialbg@gmail.com>
2023-05-11 12:20:05 +0300

    commit

---
+++ src/SessionProvider.js
@@ -0,0 +1,11 @@
+'use client';
+
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => (
+    <SessionProvider session={session}>
+        {children}
+    </SessionProvider>
+)
+
+export default Provider;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,8 @@
-'use client'
-
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
-import { SessionProvider } from 'next-auth/react'
+import Provider from '@/SessionProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,11 +15,11 @@ export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider session={session}>
+        <Provider>
           <Navbar />
           {children}
           <Footer />
-        </SessionProvider>
+        </Provider>
       </body>
     </html>
   )
